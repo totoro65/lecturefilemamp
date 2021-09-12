c22d569e590b41abb3d2c0df74efcdc2d91faa62
totoro65 <86171458+totoro65@users.noreply.github.com>
2021-09-12 14:13:22 +0200

    Create fichiera.php

---
+++ fichiera.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the OpenClassRoom PHP Object Course.
+ *
+ * (c) Anthony Gast <anthonygast19@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+class Lobby
+{
+    /** @var array<AGPlayer> */
+    public array $AGPlayers = [];
+
+    public function findOponents(AGPlayer $player): array
+    {
+        $minLevel = round($player->getRatio() / 100);
+        $maxLevel = $minLevel + $player->getRange();
+
+        return array_filter($this->agPlayers, static function (AGPlayer $potentialOponent) use ($minLevel, $maxLevel, $player) {
+            $playerLevel = round($potentialOponent->getRatio() / 100);
+
+            return $player !== $potentialOponent && ($minLevel <= $playerLevel) && ($playerLevel <= $maxLevel);
+        });
+    }
+
+    public function addPlayer(Player $player): void
+    {
+        $this->queuingPlayers[] = new AGPlayer($player);
+    }
+
+    public function addPlayers(Player ...$players): void
+    {
+        foreach ($players as $player) {
+            $this->addPlayer($player);
+        }
+    }
+}
+
+class Player
+{
+    public function __construct(protected string $name, protected float $ratio = 400.0)
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    private function probabilityAgainst(self $player): float
+    {
+        return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
+    }
+
+    public function updateRatioAgainst(self $player, int $result): void
+    {
+        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
+    }
+
+    public function getRatio(): float
+    {
+        return $this->ratio;
+    }
+}
+
+$greg = new Player('greg', 400);
+$jade = new Player('jade', 476);
+
+$lobby = new Lobby();
+$lobby->addPlayers($greg, $jade);
+
+var_dump($lobby->findOponents($lobby->AGPPlayers[0]));
+
+exit(0);
+© 2021 GitHub, Inc.
